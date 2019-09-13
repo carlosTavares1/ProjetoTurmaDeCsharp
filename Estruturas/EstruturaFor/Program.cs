d534507e61bcee34b062fd825277ccaa72881fd5
Carlos Tavares <cast.3@hotmail.com>
2019-09-13 20:14:18 -0300

    Projetos comentados

--- Estruturas/EstruturaFor/Program.cs
+++ Estruturas/EstruturaFor/Program.cs
@@ -10,16 +10,16 @@ namespace EstruturaFor
     {
         static void Main(string[] args)
         {
-            int media = 0;
-            Console.WriteLine("Informe o nome do aluno: ");
-            string nome = Console.ReadLine();
-            int[] nota = new int[4];
-            for(int i = 0; i < nota.Length; i++)
+            int media = 0;  //inicializando a variável com valor zero
+            Console.WriteLine("Informe o nome do aluno: ");     //escrevendo no console o texto entre aspas
+            string nome = Console.ReadLine();   //assinando na variável nome o que foi digitado no console
+            int[] nota = new int[4];    //declarando um vetor de quatro posições
+            for(int i = 0; i < nota.Length; i++)    //iniciando a estrutura de repetição
             {
                 Console.WriteLine($"Informe {i+1}ª avaliação: ");
                 nota[i] = int.Parse(Console.ReadLine());
                 media = media + nota[i];
-            }
+            }                                       //terminando a estrutura de repetição
             media = media / nota.Length;
             Console.WriteLine($"O aluno {nome} possui média {media}");
             Console.ReadKey();
---
+++ Estruturas/estruturaDeDecisao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estruturaDeDecisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, multiplicacao;   //declaração de variáveis na mesma linha pois são do mesmo tipo. Só pode quando são do mesmo e não são inicializadas.
+            Console.WriteLine("Informe o primeiro valor: ");    //método para escrever na tela do console
+            num1 = double.Parse(Console.ReadLine());    //atribuição do valor escrito no console. Feito a conversão do valor digitado porque ele é recuperado como string e a variável é double, então deve converter.
+            Console.WriteLine("Informe o segundo valor: ");     //método para escrever na tela do console
+            num2 = double.Parse(Console.ReadLine());    //atribuição do valor escrito no console. Feito a conversão do valor digitado porque ele é recuperado como string e a variável é double, então deve converter.
+            multiplicacao = num1 * num2;    //multiplicação dos valores recuperados e atribuídos à variável multiplicação
+            Console.WriteLine("O produto do primeiro pelo segundo é: " + multiplicacao);    //Método retorna a string seguida do resultado da variável.
+        }
+    }
+}
