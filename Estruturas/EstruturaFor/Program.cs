581fd9167a8aa42dc549b1e1a24701c4b3a2302a
Carlos Tavares <cast.3@hotmail.com>
2019-09-13 17:27:28 -0300

    aula 5

---
+++ Estruturas/EstruturaFor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int media = 0;
+            Console.WriteLine("Informe o nome do aluno: ");
+            string nome = Console.ReadLine();
+            int[] nota = new int[4];
+            for(int i = 0; i < nota.Length; i++)
+            {
+                Console.WriteLine($"Informe {i+1}ª avaliação: ");
+                nota[i] = int.Parse(Console.ReadLine());
+                media = media + nota[i];
+            }
+            media = media / nota.Length;
+            Console.WriteLine($"O aluno {nome} possui média {media}");
+            Console.ReadKey();
+        }
+
+
+        public int nome;
+        public void PegaMedia()
+        {
+            PegaMedia = nota1 + nota2;
+        }
+
+
+    }
+}
