11c8ee65730562c60b3f8b3d54a53570d3e401c9
Carlos Tavares <cast.3@hotmail.com>
2019-09-13 20:52:30 -0300

    Projetos comentados

--- Estruturas/EstruturaFor/Program.cs
+++ Estruturas/EstruturaFor/Program.cs
@@ -16,22 +16,13 @@ namespace EstruturaFor
             int[] nota = new int[4];    //declarando um vetor de quatro posições
             for(int i = 0; i < nota.Length; i++)    //iniciando a estrutura de repetição
             {
-                Console.WriteLine($"Informe {i+1}ª avaliação: ");
-                nota[i] = int.Parse(Console.ReadLine());
-                media = media + nota[i];
+                Console.WriteLine($"Informe {i+1}ª avaliação: "); //usa o método WriteLine para escrever na tela o texto e a variável i indicando o número da avaliação
+                nota[i] = int.Parse(Console.ReadLine());    //assinando um valor digitado em cada posição do vetor
+                media = media + nota[i];    //soma dos valores digitados em cada posição do vetor
             }                                       //terminando a estrutura de repetição
-            media = media / nota.Length;
-            Console.WriteLine($"O aluno {nome} possui média {media}");
-            Console.ReadKey();
+            media = media / nota.Length; //cálculo da média
+            Console.WriteLine($"O aluno {nome} possui média {media}");  //escrevendo o método através do método WriteLine
+            Console.ReadKey();  //método faz a leitura de uma tecla apenas
         }
-
-
-        public int nome;
-        public void PegaMedia()
-        {
-            PegaMedia = nota1 + nota2;
-        }
-
-
     }
 }
---
+++ Estruturas/operadorRelacional/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadorRelacional
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //sistema solicita as notas e realiza o cálculo da média e retorna se o aluno foi aprovado ou não
+            double nota1, nota2, nota3, nota4, media = 0;   //assinando as variáveis do tipo double
+            string frase = "Digite a {0}º nota: ";
+
+            Console.WriteLine(frase, 1);    //escreve a frase
+            nota1 = Convert.ToDouble(Console.ReadLine());   //assina a nota1 com o valor digitado. o Convert.ToDouble funciona como o int.Parse
+
+            Console.WriteLine(frase, 2);    //escreve a frase
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine(frase, 3);    //escreve a frase
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(frase, 4);    //escreve a frase
+            nota4 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4;    //cálcula a média entre as notas
+
+            Console.WriteLine("A média foi de " + media);   //escreve a frase concatenando com o valor da variável media
+
+            if (media < 4)  //estrutura de decisão avalia se o aluno está aprovado, reprovado ou em exame
+            {
+                Console.WriteLine("O aluno está reprovado!");
+            }
+            else
+            {
+                if (media < 7)
+                {
+                    Console.WriteLine("O aluno está em exame!");
+                }
+                else
+                {
+                    Console.WriteLine("O aluno está aprovado!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
