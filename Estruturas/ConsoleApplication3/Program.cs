928d44789d4e9567234bb49d8171b89c267e6e82
Carlos Tavares <cast.3@hotmail.com>
2019-09-13 23:16:56 -0300

    Projetos comentados

---
+++ Estruturas/ConsoleApplication3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5] { 1, 2, 3, 4, 5 };   //vetor assinado
+            foreach (int elemento in numeros)   //estrutura de repeticação. a cada repeticação ele assina o valor da posição do vetor na variável
+            {
+                Console.WriteLine(elemento);    //imprime no console o valor da posição do vetor
+            }
+            Console.ReadKey();
+        }
+    }
+}
